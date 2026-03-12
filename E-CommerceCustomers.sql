3c985bd606b33f9b687aa68107c448f72ffb1566
Mandarada Uma Maheshwari <uma.mandarada@gmail.com>
2026-03-12 20:58:04 +0530

    Add files via upload

---
+++ E-CommerceCustomers.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE CUSTOMERS
+CREATE TABLE customers (
+	customer_id INT,
+	customer_name VARCHAR(50),
+	city VARCHAR(50),
+	signup_date DATE
+);
+
+-- INSERT VALUES INTO CUSTOMERS TABLE
+INSERT INTO customers VALUES
+(1,'Ravi','Hyderabad','2023-01-10'),
+(2,'Sneha','Bangalore','2023-02-15'),
+(3,'Amit','Hyderabad','2023-03-20'),
+(4,'Priya','Chennai','2023-04-05'),
+(5,'Kiran','Pune','2023-05-01');
